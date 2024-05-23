b3dfbdd0aed96d42d20ffde2465d81a35e629846
elenanova <qa.elenanova@gmail.com>
2024-05-22 22:25:25 -0400

    task1 done

---
+++ hw3/task1.js
@@ -0,0 +1,33 @@
+// 1-3
+let item_1 = 5;
+console.log(item_1);
+
+// 4-6
+let item_2 = 3;
+console.log(item_2);
+
+// 7-9
+let item_3 = item_1 + item_2;
+console.log(item_3);
+
+// 10-12
+let item_4 = 'Yolochka';
+console.log(item_4);
+
+// 13-14
+console.log(item_3+item_4);
+console.log(item_3*item_4);
+
+// 15-16
+let item_5 = item_3;
+
+// 17-21
+let item_6, item_6_Type;
+item_6 = 15;
+item_6_Type = typeof item_6;
+console.log('item_6 == ' + item_6 + ', ' + 'item_6_Type == ' + item_6_Type);
+
+// 22-25
+let item_7 = String(item_6);
+let item_7_type = typeof item_7;
+console.log('item_7 == ' + item_7 + ', ' + 'item_7_type == ' + item_7_type );
