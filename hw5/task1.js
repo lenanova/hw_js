6abff5b1913e68084657f93c9465042d9ff63df7
elenanova <qa.elenanova@gmail.com>
2024-06-01 21:13:06 -0400

    task1 done

---
+++ hw5/task1.js
@@ -0,0 +1,23 @@
+// 0..10
+
+let i = 0;
+
+while (i <=10) {
+    console.log(i);
+    i++;
+}
+
+// :)
+const emoji = ':)';
+
+for (i = 1; i < 6; i++) {
+    console.log(emoji.repeat(i));
+}
+
+// :) while
+let counter = 1;
+
+while (counter < 6) {
+    console.log(emoji.repeat(counter));
+    counter++;
+}
