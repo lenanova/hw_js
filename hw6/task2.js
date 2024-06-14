c7e150ff8337079f8e21d711d734944c5e8028b3
elenanova <qa.elenanova@gmail.com>
2024-06-13 23:08:35 -0400

    Pizza task done

---
+++ hw6/task2.js
@@ -0,0 +1,48 @@
+/*
+1. У вас есть массив названий пицц вашего конкурента. 
+Создайте функцию, которая будет принимать ваш набор названий пицц (массив) 
+и возвращать только те, которых нет у конкурента (тоже массив). 
+Если все ваши пиццы есть у конкурента - вернуть null
+Пиццы конкурента:
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+*/
+
+// const competitorPizzas = ['Pepperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawaii']
+
+// function comparePizzas(myPizzas) {
+//     const competitorPizzasLowerCase = competitorPizzas.map(pizza => pizza.toLowerCase());
+//     const result = myPizzas.filter(pizza => competitorPizzasLowerCase.includes(pizza.toLowerCase()));
+
+//     return result.length === 0 ? null : result;
+// }
+
+// console.log(comparePizzas(['Quatro Queso', 'Tacos Pizza', 'Hawaii', 'Pepperoni']));
+
+const competitorPizzas = ['Pepperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawaii'];
+
+function comparePizzas(myPizzas) {
+  const competitorPizzasLowerCase = competitorPizzas.map(pizza => pizza.toLowerCase());
+  const result = [];
+  for (let i = 0; i < myPizzas.length; i++) {
+    if (!competitorPizzasLowerCase.includes(myPizzas[i].toLowerCase())) {
+      result.push(myPizzas[i]);
+    }
+  }
+  if (result.length === 0) {
+    return null;
+  }
+  return result;
+}
+
+console.log(comparePizzas(['Quatro Queso', 'Tacos Pizza', 'Hawaii', 'Pepperoni']));
+console.log(comparePizzas(['Hawaii', 'Pepperoni']));
+
+
+/*  
+2. Написать функцию, которая принимает предложение (слова разделенные только пробелами) в качестве параметра и выводит в консоль слово с наибольшим количеством букв. 
+  Если таких слов несколько - выводит их все.
+
+3. Напишите функцию, которая принимает на вход массив чисел, убирает из него дубликаты и возвращает массив с только уникальными значениями.
+
+4. Написать функцию, которая принимает на вход слово и проверяет, является ли это слово палиндромом
+*/
