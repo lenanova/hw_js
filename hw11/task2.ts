7c39fb6feccb6657694f80aa21d8a38ca6e3276d
elenanova <qa.elenanova@gmail.com>
2024-07-15 08:31:50 -0400

    fix typo

--- hw11/task2.ts
+++ hw11/task2.ts
@@ -149,4 +149,3 @@ function filter(numbers: Tnumbers, callbackFunction: TcallbackFunction): Tnumber
 const numbers = [1, -5, 2, 3, 4, 133];
 console.log(filter(numbers, (n) => n > 3));
 console.log(filter(numbers, (n) => n % 2 === 0));
-
